042db396909060df7a7fb8cfad15597897dc8e91
Abel Murua <abelmurua87@gmail.com>
2023-12-12 01:19:14 -0300

    arreglos

--- src/controllers/cuotas.js
+++ src/controllers/cuotas.js
@@ -1,108 +1,100 @@
 const Cuota = require("../models/cuotas");
 
 const getStatus = (req, res) => {
-    Cuota.find()
-        .then((response) => res.status(200).json({ msg: "Connection OK" }))
-        .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
-}
+  Cuota.find()
+    .then((response) => res.status(200).json({ msg: "Connection OK" }))
+    .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
+};
 
 // Devuelve todos los Cuotas
 const getAll = async (req, res) => {
+  let cuotas = [];
 
-    let cuotas = [];
+  try {
+    cuotas = await Cuota.find({});
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.json({ msg: `Error: ${error}` });
+  }
 
-    try {
-        cuotas = await Cuota.find({})
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-        res.json({ msg: `Error: ${error}` });
-    }
-
-    // solo devolvemos los cuotas si no se entro al catch
-    res.json(cuotas);
+  // solo devolvemos los cuotas si no se entro al catch
+  res.json(cuotas);
 };
 
 //GET by ID
 const getCuotaById = (req, res) => {
-    const { CuotaId } = req.params;
-    Cuota.find({ id: CuotaId })
-        .then((data) => res.json({ data }))
-        .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
-}
+  const { CuotaId } = req.params;
+  Cuota.find({ id: CuotaId })
+    .then((data) => res.json({ data }))
+    .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
+};
 
 //POST
 
 const create = async (req, res) => {
-    const payload = req.body;
-    console.log("🚀 ~ file: cuotas.js:39 ~ create ~ payload:", payload)
+  const payload = req.body;
+  console.log("🚀 ~ file: cuotas.js:39 ~ create ~ payload:", payload);
 
-    const cuota = new Cuota(payload);
+  const cuota = new Cuota(payload);
 
-    let savedCuota;
-    try {
-        savedCuota = await cuota.save();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Post: ${err}` });
-    }
+  let savedCuota;
+  try {
+    savedCuota = await cuota.save();
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Post: ${err}` });
+  }
 
-    res.json(savedCuota);
-
+  res.json(savedCuota);
 };
 
 // UPDATE de cuota
 actualizarCuota = async (req, res) => {
-    const id = req.params.id;
-    const payload = req.body;
-    console.log("🚀 ~ file: cuotas.js:59 ~ actualizarCuota= ~ payload:", payload)
+  const id = req.params.id;
+  const payload = req.body;
 
-    let cuotaAct;
-    try {
-        cuotaAct = await Cuota.updateOne(
-            { "id": id },
-            {
-                $set: payload
-            }
-        );
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Update: ${err}` });
-    }
+  let cuotaAct;
+  try {
+    cuotaAct = await Cuota.findOneAndUpdate(
+      { id: id },
+      { $set: payload },
+      { returnDocument: "after" }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Update: ${err}` });
+  }
 
-    res.json(cuotaAct);
+  res.json(cuotaAct);
 };
 
 // DELETE de cuota
 const eliminarCuota = async (req, res) => {
-    const id = req.params.id;
-    let response;
-    try {
-        response = await Cuota.deleteOne({ id });
-        console.log(response);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Delete: ${err}` });
-    }
-    if (response.deletedCount === 0) {
-        return res.json({ msg: `No se encontro cuota con id: ${id}` });
-    }
+  const id = req.params.id;
+  let response;
+  try {
+    response = await Cuota.deleteOne({ id });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Delete: ${err}` });
+  }
+  if (response.deletedCount === 0) {
+    return res.json({ msg: `No se encontro cuota con id: ${id}` });
+  }
 
-    return res.json({ msg: `La cuota fue borrada ${id}` });
-}
-
+  return res.json({ msg: `La cuota fue borrada ${id}` });
+};
 
 module.exports = {
-    getStatus,
-    getAll,
-    getCuotaById,
-    create,
-    actualizarCuota,
-    eliminarCuota,
+  getStatus,
+  getAll,
+  getCuotaById,
+  create,
+  actualizarCuota,
+  eliminarCuota,
 };
--- src/controllers/socios.js
+++ src/controllers/socios.js
@@ -3,185 +3,171 @@ const Cuota = require("../models/cuotas");
 const { json } = require("express");
 
 const getStatus = (req, res) => {
-    Socio.find()
-        .then((response) => res.status(200).json({ msg: "Connection OK" }))
-        .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
-}
+  Socio.find()
+    .then((response) => res.status(200).json({ msg: "Connection OK" }))
+    .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
+};
 
 // Devuelve todos los socios
 const getAll = async (req, res) => {
+  let socios = [];
 
-    let socios = [];
+  try {
+    socios = await Socio.find({}, { isDeleted: 0, __v: 0, _id: 0 });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.json({ msg: `Error: ${error}` });
+  }
 
-    try {
-        socios = await Socio.find({}, { isDeleted: 0, __v: 0, _id: 0 })
-        console.log("🚀 ~ file: socios.js:16 ~ getAll ~ socios:", socios)
-    } catch (error) {
-        console.log(error);
-        res.status(500);
-        res.json({ msg: `Error: ${error}` });
-    }
-
-    // solo devolvemos los socios si no se entro al catch
-    res.json(socios);
+  // solo devolvemos los socios si no se entro al catch
+  res.json(socios);
 };
 
 //GET by ID
 const getSocioById = (req, res) => {
-    const { SocioId } = req.params;
-    Socio.find({ id: SocioId })
-        .then((data) => res.json({ data }))
-        .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
-}
+  const { SocioId } = req.params;
+  Socio.find({ id: SocioId })
+    .then((data) => res.json({ data }))
+    .catch((err) => res.status(500).json({ msg: `Error: ${err}` }));
+};
 
 //POST
 
 const create = async (req, res) => {
+  const { id, dni, name, lastname, tel, mail, fechaNac } = req.body;
 
-    const { id, dni, name, lastname, tel, mail, fechaNac } = req.body;
+  const socio = new Socio({
+    id,
+    dni,
+    name,
+    lastname,
+    tel,
+    mail,
+    fechaNac,
+  });
+  let savedSocio;
+  try {
+    savedSocio = await socio.save();
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Post: ${err}` });
+  }
 
-    const socio = new Socio({
-        id,
-        dni,
-        name,
-        lastname,
-        tel,
-        mail,
-        fechaNac,
-
-    });
-    let savedSocio;
-    try {
-        savedSocio = await socio.save();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Post: ${err}` });
-    }
-
-    res.json(savedSocio);
-
+  res.json(savedSocio);
 };
 
 // UPDATE de socios
 const actualizarSoc = async (req, res) => {
-    const id = req.params.id;
-    const { dni, name, lastname, tel, mail, fechaNac } = req.body;
-    console.log(id);
+  const id = req.params.id;
+  const { dni, name, lastname, tel, mail, fechaNac } = req.body;
+  console.log(id);
 
-    let socioAct;
-    try {
-        socioAct = await Socio.updateOne(
-            { "id": id },
-            {
-                $set: {
-                    dni: dni,
-                    name: name,
-                    lastname: lastname,
-                    tel: tel,
-                    mail: mail,
-                    fechaNac: fechaNac
-                }
-            }
-        );
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Update: ${err}` });
-    }
+  let socioAct;
+  try {
+    socioAct = await Socio.updateOne(
+      { id: id },
+      {
+        $set: {
+          dni: dni,
+          name: name,
+          lastname: lastname,
+          tel: tel,
+          mail: mail,
+          fechaNac: fechaNac,
+        },
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Update: ${err}` });
+  }
 
-    res.json(socioAct);
+  res.json(socioAct);
 };
 
 // DELETE de socios
 const eliminarSoc = async (req, res) => {
-    const id = req.params.id;
-    let response;
-    try {
-        response = await Socio.deleteOne({ id });
-        console.log(response);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Delete: ${err}` });
-    }
-    if (response.deletedCount === 0) {
-        return res.json({ msg: `No se encontro socio con id: ${id}` });
-    }
+  const id = req.params.id;
+  let response;
+  try {
+    response = await Socio.deleteOne({ id });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Delete: ${err}` });
+  }
+  if (response.deletedCount === 0) {
+    return res.json({ msg: `No se encontro socio con id: ${id}` });
+  }
 
-    return res.json({ msg: `El socio borrado ${id}` });
-}
+  return res.json({ msg: `El socio borrado ${id}` });
+};
 
 //Controla acceso
 const accesoSocio = async (req, res) => {
-    const { socio } = req.body;
+  const { socio } = req.body;
 
-    let foundCuota;
-    let resultAccess = false;
+  let foundCuota;
+  let resultAccess = false;
 
-    try {
-        // hacer un GET de mongoDB con filtro por DNI a la collection cuota
-        foundCuota = await Cuota.findOne({ socio }); // socio, es un DNI
+  try {
+    // hacer un GET de mongoDB con filtro por DNI a la collection cuota
+    foundCuota = await Cuota.findOne({ socio }); // socio, es un DNI
 
-        if (foundCuota) {
-            const tipoCuotaValidadoMinuscula = foundCuota.tipo.toLowerCase();
+    if (foundCuota) {
+      const tipoCuotaValidadoMinuscula = foundCuota.tipo.toLowerCase();
 
-            // vamos a crear fecha de hoy antes porque la vamso a usar
-            const dateNow = new Date();
+      // vamos a crear fecha de hoy antes porque la vamso a usar
+      const dateNow = new Date();
 
-            const calculateDate = (createdAt, days) => {
+      const calculateDate = (createdAt, days) => {
+        const dueDate = new Date(createdAt.setDate(createdAt.getDate() + days));
 
-                const dueDate = new Date(createdAt.setDate(createdAt.getDate() + days))
+        // devolvemos false para que no peuda acceder si ya se vencio.
+        const acceso = dueDate < dateNow ? false : true;
+        return acceso;
+      };
 
-                // devolvemos false para que no peuda acceder si ya se vencio.
-                const acceso = dueDate < dateNow ? false : true;
-                return acceso;
-            }
+      switch (tipoCuotaValidadoMinuscula) {
+        case "mensual":
+          // aplicamos y devolvemo o true o false si tiene acceso
+          resultAccess = calculateDate(foundCuota.created_at, 31);
 
+        case "trimestral":
+          // aplicamos y devolvemo o true o false si tiene acceso
+          resultAccess = calculateDate(foundCuota.created_at, 90);
 
+        case "semestral":
+          // aplicamos y devolvemo o true o false si tiene acceso
+          resultAccess = calculateDate(foundCuota.created_at, 120);
 
-            switch (tipoCuotaValidadoMinuscula) {
-                case "mensual":
-                    // aplicamos y devolvemo o true o false si tiene acceso
-                    resultAccess = calculateDate(foundCuota.created_at, 31);
-
-                case "trimestral":
-                    // aplicamos y devolvemo o true o false si tiene acceso
-                    resultAccess = calculateDate(foundCuota.created_at, 90);
-
-                case "semestral":
-                    // aplicamos y devolvemo o true o false si tiene acceso
-                    resultAccess = calculateDate(foundCuota.created_at, 120);
-
-                default:
-                    // aplicamos y devolvemo o true o false si tiene acceso
-                    resultAccess = calculateDate(foundCuota.created_at, 31);
-            }
-        }
-        else {
-            return res.json("not found")
-        }
+        default:
+          // aplicamos y devolvemo o true o false si tiene acceso
+          resultAccess = calculateDate(foundCuota.created_at, 31);
+      }
+    } else {
+      return res.json("not found");
     }
-    catch (err) {
-        console.log("🚀 ~ file: socios.js:171 ~ accesoSocio ~ err:", err)
-        console.log(err);
-        res.status(500);
-        res.json({ msg: `Error Post: ${err}` });
-    }
+  } catch (err) {
+    console.log("🚀 ~ file: socios.js:171 ~ accesoSocio ~ err:", err);
+    console.log(err);
+    res.status(500);
+    res.json({ msg: `Error Post: ${err}` });
+  }
 
-    return res.json(resultAccess);
+  return res.json(resultAccess);
 };
 
-
 module.exports = {
-    getStatus,
-    getAll,
-    getSocioById,
-    create,
-    actualizarSoc,
-    eliminarSoc,
-    accesoSocio
+  getStatus,
+  getAll,
+  getSocioById,
+  create,
+  actualizarSoc,
+  eliminarSoc,
+  accesoSocio,
 };
